fca1ef61e5e5035978e87e7c6cf4c94e733a819a
WMHillock <bugorskiyvv@gmail.com>
2023-05-16 00:20:49 +0400

    add 7kyu - Isograms

---
+++ src/main/java/difficulty/kyu7/Isograms.java
@@ -0,0 +1,10 @@
+package difficulty.kyu7;
+
+public class Isograms {
+    public static boolean  isIsogram(String str) {
+        return str.toLowerCase()
+                .chars()
+                .distinct()
+                .count() == str.length();
+    }
+}
