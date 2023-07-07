151201b34cb3cb6b2fadc4191f083bce9096d635
Vladimir Bugorskiy <53443987+WMHillock@users.noreply.github.com>
2023-07-07 13:30:21 +0400

    Вынесение текущих нерешенных задач в отдельную ветку  -> feature/ParseFromTextToInt

--- src/main/java/difficulty/kyu4/ParseFromTextToInt.java
+++ src/main/java/difficulty/kyu4/ParseFromTextToInt.java
@@ -1,6 +1,7 @@
 package difficulty.kyu4;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ParseFromTextToInt {
     public static void main(String[] args) {
@@ -34,32 +35,41 @@ public class ParseFromTextToInt {
 
     //TODO добавить методы - сверяющий со словарем возвращающий число, методы * и +
     public static HashMap<String, Integer> vokabulary() {
-        HashMap<String, Integer> wordsToIntDictionary = new HashMap<>();
-        //Значения для поиска
-        wordsToIntDictionary.put("zer", 0);
-        wordsToIntDictionary.put("one",1);
-        wordsToIntDictionary.put("two", 2);
-        wordsToIntDictionary.put("thr", 3);
-        wordsToIntDictionary.put("fo", 4);
-        wordsToIntDictionary.put("fi", 5);
-        wordsToIntDictionary.put("six", 6);
-        wordsToIntDictionary.put("sev", 7);
-        wordsToIntDictionary.put("eig", 8);
-        wordsToIntDictionary.put("nin", 9);
-        wordsToIntDictionary.put("ten", 10);
-        wordsToIntDictionary.put("ele",11);
-        wordsToIntDictionary.put("twel", 12);
 
-        //Слагаемые и мультипликаторы
-        wordsToIntDictionary.put("ty", 10);         //*10
-        wordsToIntDictionary.put("teen", 10);       //+10
-        wordsToIntDictionary.put("hund",100);       //*100
-        wordsToIntDictionary.put("thous", 100);     //*1000
-        wordsToIntDictionary.put("mil", 1_000_000); //*1_000_000
-
-        return wordsToIntDictionary;
+            HashMap<String, Integer> map = new HashMap<>();
+            map.put("zero", 0);
+            map.put("one", 1);
+            map.put("two", 2);
+            map.put("three", 3);
+            map.put("four", 4);
+            map.put("five", 5);
+            map.put("six", 6);
+            map.put("seven", 7);
+            map.put("eight", 8);
+            map.put("nine", 9);
+            map.put("ten", 10);
+            map.put("eleven", 11);
+            map.put("twelve", 12);
+            map.put("thirteen", 13);
+            map.put("fourteen", 14);
+            map.put("fifteen", 15);
+            map.put("sixteen", 16);
+            map.put("seventeen", 17);
+            map.put("eighteen", 18);
+            map.put("nineteen", 19);
+            map.put("twenty", 20);
+            map.put("thirty", 30);
+            map.put("forty", 40);
+            map.put("fifty", 50);
+            map.put("sixty", 60);
+            map.put("seventy", 70);
+            map.put("eighty", 80);
+            map.put("ninety", 90);
+            map.put("hundred", 100);
+            map.put("thousand", 1000);
+            map.put("million", 1000000);
+            map.put("billion", 1000000000);
+            return map;
+        }
     }
 
-
-
-}
