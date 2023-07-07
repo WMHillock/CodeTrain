1d015da6f0cd6f035b7962b5ce003e96728d9035
Vladimir Bugorskiy <53443987+WMHillock@users.noreply.github.com>
2023-07-07 11:53:47 +0400

    add 7kyu - LeapYear add 4kyu - ParseFromTextToInt

---
+++ src/main/java/difficulty/kyu7/LeapYear.java
@@ -0,0 +1,12 @@
+package difficulty.kyu7;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1900));
+    }
+
+    public static boolean isLeapYear(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
+}
